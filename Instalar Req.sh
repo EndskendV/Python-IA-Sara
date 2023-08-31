2df180b7e46d91802f9b19053b683a8d328ac89d
EndskendV <endskend@gmail.com>
2023-08-31 15:14:13 -0700

    f

---
+++ Instalar Req.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+
+sudo apt-get -y install python3-pip
+
+# Instalacion de PIP para instalar requerimientos.
+
+cd "Asistente virtual (Sara)/"
+
+python3 -m pip install -r "requeriments.txt"
